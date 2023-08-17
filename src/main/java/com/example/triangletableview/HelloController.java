0480bbb498c4a60570d62ebd9fbd5b9e9150f366
alkaz07 <>
2023-08-17 11:16:37 +0300

    result version

--- src/main/java/com/example/triangletableview/HelloController.java
+++ src/main/java/com/example/triangletableview/HelloController.java
@@ -1,14 +1,89 @@
 package com.example.triangletableview;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.converter.DoubleStringConverter;
 
+import java.io.*;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import static java.nio.file.Files.readAllLines;
+
+/*
+сделать графическое приложение, которое грузит из файла список Треугольников
+каждый треугольник задан 3 сторонами
+Вывести в табличку или отдельными элементами все треугольники с указанием площади
+
++ делаем строки таблицы редактируемыми и даем возможность сохранить изменения в файл
+ */
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    TableView<ObservableTriangle> table;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    Button saveBtn;
+    ObservableList<ObservableTriangle> triangles = FXCollections.observableArrayList();
+
+    public void initialize() throws IOException {
+        readTrianglesFromFile();
+        initTable();
     }
+    public void readTrianglesFromFile() throws IOException {
+        List<String> list = readAllLines(new File("1.txt").toPath(), Charset.forName("UTF-8"));
+        loadTriangles(list);
+    }
+    public void loadTriangles(List<String> lines){
+        for (String str: lines) {
+            ObservableTriangle oTr = new ObservableTriangle(str.split(" ")[0], str.split(" ")[1], str.split(" ")[2]);
+            triangles.add(oTr);
+        }
+    }
+    public void initTable()
+    {
+        table.getColumns().clear();
+        TableColumn<ObservableTriangle, Double> columnA = new TableColumn<>("A");
+        columnA.setCellValueFactory(new PropertyValueFactory<>("sideA"));
+        columnA.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));
+
+
+        TableColumn<ObservableTriangle, Double> columnB = new TableColumn<>("B");
+        columnB.setCellValueFactory(new PropertyValueFactory<>("sideB"));
+        columnB.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));
+
+        TableColumn<ObservableTriangle, Double> columnC = new TableColumn<>("C");
+        columnC.setCellValueFactory(new PropertyValueFactory<>("sideC"));
+        columnC.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));
+
+        TableColumn<ObservableTriangle, Double> columnArea = new TableColumn<>("Area");
+        columnArea.setCellValueFactory(new PropertyValueFactory<>("area"));
+        //columnArea.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));
+
+
+
+        table.getColumns().add(columnA);
+        table.getColumns().add(columnB);
+        table.getColumns().add(columnC);
+        table.getColumns().add(columnArea);
+        table.setItems(triangles);
+        table.setEditable(true);
+
+
+    }
+
+
+//    @FXML
+//    private Label welcomeText;
+//
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
 }
--- src/main/java/com/example/triangletableview/ObservableTriangle.java
+++ src/main/java/com/example/triangletableview/ObservableTriangle.java
@@ -1,2 +1,134 @@
-package com.example.triangletableview;public class ObservableTriangle {
+package com.example.triangletableview;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.List;
+
+public class ObservableTriangle {
+    ObservableList<ObservableTriangle> triangles = FXCollections.observableArrayList();
+
+    SimpleDoubleProperty sideA = new SimpleDoubleProperty();
+    SimpleDoubleProperty sideB = new SimpleDoubleProperty();
+    SimpleDoubleProperty sideC = new SimpleDoubleProperty();
+
+    private SimpleDoubleProperty perimeter = new SimpleDoubleProperty();
+    private SimpleDoubleProperty area = new SimpleDoubleProperty();
+
+    {//секция инициализации
+        this.sideA.addListener((s1, o,n)->{
+            this.perimeter.set(perimeter());
+            this.area.set(area());
+        });
+        this.sideB.addListener((s1, o,n)->{
+            this.perimeter.set(perimeter());
+            this.area.set(area());
+        });
+        this.sideC.addListener((s1, o,n)->{
+            this.perimeter.set(perimeter());
+            this.area.set(area());
+        });
+    }
+
+
+    public double getSideA() {
+        return sideA.get();
+    }
+
+    public SimpleDoubleProperty sideAProperty() {
+        return sideA;
+    }
+
+    public void setSideA(double sideA) {
+        this.sideA.set(sideA);
+      // area.set(area());
+    }
+
+    public double getSideB() {
+        return sideB.get();
+    }
+
+    public SimpleDoubleProperty sideBProperty() {
+        return sideB;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB.set(sideB);
+    }
+
+    public double getSideC() {
+        return sideC.get();
+    }
+
+    public SimpleDoubleProperty sideCProperty() {
+        return sideC;
+    }
+
+    public void setSideC(double sideC) {
+        this.sideC.set(sideC);
+    }
+
+    public double getPerimeter() {
+        return perimeter.get();
+    }
+
+    public SimpleDoubleProperty perimeterProperty() {
+        return perimeter;
+    }
+
+   // public void setPerimeter(double perimeter) {
+   //     this.perimeter.set(perimeter);
+    //}
+
+    public double getArea() {
+        return area.get();
+    }
+
+    public SimpleDoubleProperty areaProperty() {
+        return area;
+    }
+
+//    public void setArea(double area) {
+//        this.area.set(area);
+//    }
+
+
+    public ObservableTriangle(SimpleDoubleProperty sideA, SimpleDoubleProperty sideB, SimpleDoubleProperty sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public ObservableTriangle(String sideA, String sideB, String sideC) {
+        this.sideA.set(Double.parseDouble(sideA));
+        this.sideB.set(Double.parseDouble(sideB));
+        this.sideC.set(Double.parseDouble(sideC));
+    }
+
+    public ObservableTriangle(double a, double b, double c) {
+        setSideA(a);
+        setSideB(b);
+        setSideC(c);
+    }
+
+    double perimeter()
+    {
+        return sideA.get()+sideB.get()+sideC.get();
+    }
+    double area()
+    {
+        double p = perimeter.get()/2;
+        return Math.sqrt(p*(p-sideA.get())*(p-sideB.get())*(p-sideC.get()));
+    }
+
+    public void loadTriangles(List<String> lines){
+        for (String str: lines) {
+            ObservableTriangle oTr = new ObservableTriangle(str.split(" ")[0], str.split(" ")[1], str.split(" ")[2]);
+            triangles.add(oTr);
+        }
+    }
+
+
+
 }
